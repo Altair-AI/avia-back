629d2fc5f34789a9ff2a3396221ae2f27a5b4cd6
LedZeppe1in <tualatin32@mail.ru>
2023-09-15 18:06:28 +0800

    Added swagger-annotations for auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,6 +24,47 @@ class AuthController extends Controller
      * @param Request $request
      * @return JsonResponse
      * @throws ValidationException
+     *
+     * @OA\Post(
+     *     path="/api/auth/login",
+     *     summary="Вход в систему",
+     *     tags={"Аутентификация"},
+     *
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             allOf={
+     *                 @OA\Schema(
+     *                     @OA\Property(property="email", type="string", example="Some email"),
+     *                     @OA\Property(property="password", type="string", example="Some password")
+     *                 )
+     *             }
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_token", type="string", example="Generated new token"),
+     *             @OA\Property(property="token_type", type="string", example="bearer"),
+     *             @OA\Property(property="expires_in", type="integer", example=3600),
+     *             @OA\Property(property="user", type="object",
+     *                 @OA\Property(property="id", type="integer", example=1),
+     *                 @OA\Property(property="name", type="string", example="Some name"),
+     *                 @OA\Property(property="email", type="string", example="Some email"),
+     *                 @OA\Property(property="password", type="string", example="Some password"),
+     *                 @OA\Property(property="role", type="integer", example=0),
+     *                 @OA\Property(property="status", type="integer", example=0),
+     *                 @OA\Property(property="full_name", type="string", example="Some full name"),
+     *                 @OA\Property(property="last_login_date", type="datetime", example="2023-09-15T01:52:11.000000Z"),
+     *                 @OA\Property(property="login_ip", type="string", example="Some IP"),
+     *                 @OA\Property(property="organization_id", type="integer", example=1),
+     *                 @OA\Property(property="created_at", type="datetime", example="2023-09-15T01:52:11.000000Z"),
+     *                 @OA\Property(property="updated_at", type="datetime", example="2023-09-15T01:52:11.000000Z")
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function login(Request $request) {
     	$validator = Validator::make($request->all(), [
@@ -41,6 +82,21 @@ class AuthController extends Controller
      * Log the user out (Invalidate the token).
      *
      * @return JsonResponse
+     *
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     summary="Выход из системы",
+     *     tags={"Аутентификация"},
+     *     security={{ "bearerAuth": {} }},
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="User successfully signed out.")
+     *         )
+     *     )
+     * )
      */
     public function logout() {
         auth()->logout();
