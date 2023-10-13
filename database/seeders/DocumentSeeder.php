e8b7d333f182a5a2f900132d471571839dcd97d2
LedZeppe1in <tualatin32@mail.ru>
2023-10-13 17:11:27 +0800

    Added document seeder

---
+++ database/seeders/DocumentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Document;
+use App\Models\TechnicalSystem;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DocumentSeeder extends Seeder
+{
+    public function run()
+    {
+        // Создание документа
+        DB::table('document')->insert([
+            'code' => 'Уникальный код документа',
+            'name' => 'Название документа',
+            'type' => Document::TROUBLESHOOTING_GUIDE_TYPE,
+            'version' => 'Версия документа',
+            'file' => 'Путь к файлу документа',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        // Создание связи документа с технической системой (самолетом)
+        DB::table('technical_system_document')->insert([
+            'document_id' => Document::all()->first()->id,
+            'technical_system_id' => TechnicalSystem::all()->first()->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
