f9893b078a8833cb2c33a456eea9c45ba746d006
LedZeppe1in <tualatin32@mail.ru>
2023-12-18 18:21:56 +0800

    Added date to info

--- app/Http/Controllers/Swagger/MainController.php
+++ app/Http/Controllers/Swagger/MainController.php
@@ -8,6 +8,7 @@ use Illuminate\Routing\Controller;
  * @OA\Info(
  *     title="Avia-back API",
  *     version="1.1.2",
+ *     description="Дата обновления: 18.12.2023"
  * ),
  *
  * @OA\PathItem(
