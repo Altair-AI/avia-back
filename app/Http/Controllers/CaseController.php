9ca0d23a79e2e1f985e62f069bcb50da7044ebcd
LedZeppe1in <tualatin32@mail.ru>
2024-02-07 10:12:14 +0800

    Added new controller, route, filter, policy and requests for cases

---
+++ app/Http/Controllers/CaseController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Filters\CaseFilter;
+use App\Http\Requests\ECase\IndexCaseRequest;
+use App\Models\CaseBasedKnowledgeBase;
+use App\Models\ECase;
+use App\Http\Requests\ECase\StoreCaseRequest;
+use App\Http\Requests\ECase\UpdateCaseRequest;
+use App\Models\RealTimeTechnicalSystemUser;
+use App\Models\User;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Http\JsonResponse;
+
+class CaseController extends Controller
+{
+    /**
+     * Create a new ECaseController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(ECase::class, 'case');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param IndexCaseRequest $request
+     * @return JsonResponse
+     * @throws BindingResolutionException
+     */
+    public function index(IndexCaseRequest $request)
+    {
+        $validated = $request->validated();
+        $filter = app()->make(CaseFilter::class, ['queryParams' => array_filter($validated, 'strlen')]);
+
+        $pageSize = 10;
+        if (isset($request['pageSize']))
+            $pageSize = $request['pageSize'];
+
+        $result = [];
+        $cases = [];
+        if (auth()->user()->role === User::SUPER_ADMIN_ROLE)
+            $cases = ECase::filter($filter)->paginate($pageSize);
+        if (auth()->user()->role === User::ADMIN_ROLE) {
+            // Формирование массива идентификаторов проектов в которых состоит администратор
+            $project_ids = [];
+            foreach (auth()->user()->organization->projects as $project)
+                array_push($project_ids, $project->id);
+            // Формирование id баз знаний прецедентов совпадающих с массивом идентификаторов проектов
+            $case_based_kb_ids = CaseBasedKnowledgeBase::select('id')
+                ->whereIn('project_id', $project_ids)->get();
+            // Выбираем только те прецеденты, которые доступны в рамках определенной базы знаний прецедентов
+            $cases = ECase::whereIn('case_based_knowledge_base_id', $case_based_kb_ids)
+                ->filter($filter)
+                ->paginate($pageSize);
+        }
+        if (auth()->user()->role === User::TECHNICIAN_ROLE) {
+            // Формирование массива идентификаторов технических систем реального времени, доступных технику
+            $real_time_tech_system_ids = RealTimeTechnicalSystemUser::select('real_time_technical_system_id')
+                ->where('user_id', auth()->user()->id)
+                ->get();
+            // Формирование id баз знаний прецедентов совпадающих с массивом идентификаторов технических систем реального времени
+            $case_based_kb_ids = CaseBasedKnowledgeBase::select('id')
+                ->whereIn('real_time_technical_system_id', $real_time_tech_system_ids)->get();
+            // Выбираем только те прецеденты, которые доступны в рамках определенной базы знаний прецедентов
+            $cases = ECase::whereIn('case_based_knowledge_base_id', $case_based_kb_ids)
+                ->filter($filter)
+                ->paginate($pageSize);
+        }
+        $data = [];
+        foreach ($cases as $case)
+            array_push($data, array_merge($case->toArray(), [
+                'malfunction_detection_stage' => $case->malfunction_detection_stage,
+                'malfunction_cause' => $case->malfunction_cause,
+                'system_for_repair' => $case->system_for_repair,
+                'initial_completed_operation' => $case->initial_completed_operation,
+                'case_based_knowledge_base' => $case->case_based_knowledge_base
+            ]));
+        $result['data'] = $data;
+        $result['page_current'] = !is_array($cases) ? $cases->currentPage() : null;
+        $result['page_total'] = !is_array($cases) ? $cases->lastPage() : null;
+        $result['page_size'] = !is_array($cases) ? $cases->perPage() : null;
+        return response()->json($result);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreCaseRequest $request
+     * @return JsonResponse
+     */
+    public function store(StoreCaseRequest $request)
+    {
+        $validated = $request->validated();
+        $case = ECase::create($validated);
+        return response()->json($case);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param ECase $case
+     * @return JsonResponse
+     */
+    public function show(ECase $case)
+    {
+        return response()->json(array_merge($case->toArray(), [
+            'malfunction_detection_stage' => $case->malfunction_detection_stage,
+            'malfunction_cause' => $case->malfunction_cause,
+            'system_for_repair' => $case->system_for_repair,
+            'initial_completed_operation' => $case->initial_completed_operation,
+            'case_based_knowledge_base' => $case->case_based_knowledge_base
+        ]));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateCaseRequest $request
+     * @param ECase $case
+     * @return JsonResponse
+     */
+    public function update(UpdateCaseRequest $request, ECase $case)
+    {
+        $validated = $request->validated();
+        $case->fill($validated);
+        $case->save();
+        return response()->json($case);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param ECase $case
+     * @return JsonResponse
+     */
+    public function destroy(ECase $case)
+    {
+        $case->delete();
+        return response()->json(['id' => $case->id], 200);
+    }
+}
---
+++ app/Http/Filters/CaseFilter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class CaseFilter extends AbstractFilter
+{
+    public const DATE = 'date';
+    public const CARD_NUMBER = 'card_number';
+    public const MALFUNCTION_DETECTION_STAGE_ID = 'malfunction_detection_stage_id';
+    public const MALFUNCTION_CAUSE_ID = 'malfunction_cause_id';
+    public const SYSTEM_ID_FOR_REPAIR = 'system_id_for_repair';
+    public const INITIAL_COMPLETED_OPERATION_ID = 'initial_completed_operation_id';
+    public const CASE_BASED_KNOWLEDGE_BASE_ID = 'case_based_knowledge_base_id';
+
+    protected function getCallbacks(): array
+    {
+        return [
+            self::DATE => [$this, 'date'],
+            self::CARD_NUMBER => [$this, 'cardNumber'],
+            self::MALFUNCTION_DETECTION_STAGE_ID => [$this, 'malfunctionDetectionStageId'],
+            self::MALFUNCTION_CAUSE_ID => [$this, 'malfunctionCauseId'],
+            self::SYSTEM_ID_FOR_REPAIR => [$this, 'systemIdForRepair'],
+            self::INITIAL_COMPLETED_OPERATION_ID => [$this, 'initialCompletedOperationId'],
+            self::CASE_BASED_KNOWLEDGE_BASE_ID => [$this, 'caseBasedKnowledgeBaseId']
+        ];
+    }
+
+    public function date(Builder $builder, $value)
+    {
+        $builder->where(self::DATE, $value);
+    }
+
+    public function cardNumber(Builder $builder, $value)
+    {
+        $builder->where(self::CARD_NUMBER, $value);
+    }
+
+    public function malfunctionDetectionStageId(Builder $builder, $value)
+    {
+        $builder->where(self::MALFUNCTION_DETECTION_STAGE_ID, $value);
+    }
+
+    public function malfunctionCauseId(Builder $builder, $value)
+    {
+        $builder->where(self::MALFUNCTION_CAUSE_ID, $value);
+    }
+
+    public function systemIdForRepair(Builder $builder, $value)
+    {
+        $builder->where(self::SYSTEM_ID_FOR_REPAIR, $value);
+    }
+
+    public function initialCompletedOperationId(Builder $builder, $value)
+    {
+        $builder->where(self::INITIAL_COMPLETED_OPERATION_ID, $value);
+    }
+
+    public function caseBasedKnowledgeBaseId(Builder $builder, $value)
+    {
+        $builder->where(self::CASE_BASED_KNOWLEDGE_BASE_ID, $value);
+    }
+}
