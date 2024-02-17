aa649b510eb564b4169d51b82250c678787a7187
LedZeppe1in <tualatin32@mail.ru>
2024-02-17 17:17:57 +0800

    Added new resources for operation and operation results

---
+++ app/Http/Resources/Operation/SubOperationResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Operation;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed id
+ * @property mixed verbal_name
+ * @property mixed imperative_name
+ * @property mixed operations
+ */
+class SubOperationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->imperative_name != "" ? $this->imperative_name : $this->verbal_name,
+            'status' => null,
+            'result' => null,
+            'sub_operations' => []
+        ];
+    }
+}
---
+++ app/Http/Resources/OperationResult/OperationResultResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\OperationResult;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed id
+ * @property mixed name
+ */
+class OperationResultResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name
+        ];
+    }
+}
