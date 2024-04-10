f5e55efa02f79b7dd3137317f422df63d27b72cf
LedZeppe1in <tualatin32@mail.ru>
2024-04-10 11:44:35 +0800

    Minor fixed systems.csv and real time technical system seeder

--- database/seeders/RealTimeTechnicalSystemSeeder.php
+++ database/seeders/RealTimeTechnicalSystemSeeder.php
@@ -13,10 +13,20 @@ class RealTimeTechnicalSystemSeeder extends Seeder
 {
     public function run()
     {
-        // Создание тестовой технической системы реального времени и ее подсистем
-        $real_time_technical_system_id = DB::table('real_time_technical_system')->insertGetId([
-            'registration_code' => 'Уникальный код технической системы реального времени',
-            'registration_description' => 'Дополнительное описание технической системы реального времени',
+        // Создание технических систем реального времени
+        $first_system_id = DB::table('real_time_technical_system')->insertGetId([
+            'registration_code' => 'RA-89077',
+            'registration_description' => 'IAE333 ULLI UBBB',
+            'operation_time_from_start' => 0,
+            'operation_time_from_last_repair' => 0,
+            'technical_system_id' => TechnicalSystem::first()->id,
+            'project_id' => Project::first()->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        $second_system_id = DB::table('real_time_technical_system')->insertGetId([
+            'registration_code' => 'RA-89078',
+            'registration_description' => 'IAE4141 UUDD UACC',
             'operation_time_from_start' => 0,
             'operation_time_from_last_repair' => 0,
             'technical_system_id' => TechnicalSystem::first()->id,
@@ -26,7 +36,13 @@ class RealTimeTechnicalSystemSeeder extends Seeder
         ]);
         // Создание доступа пользователей (техников) к реальным техническим системам
         DB::table('real_time_technical_system_user')->insert([
-            'real_time_technical_system_id' => $real_time_technical_system_id,
+            'real_time_technical_system_id' => $first_system_id,
+            'user_id' => User::whereId(3)->first()->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('real_time_technical_system_user')->insert([
+            'real_time_technical_system_id' => $second_system_id,
             'user_id' => User::whereId(3)->first()->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
