970e60324fe9bb3cbb52953029453b5bbd2dc8fe
LedZeppe1in <tualatin32@mail.ru>
2024-05-01 09:14:47 +0800

    Added new swagger API documentation for completed operations

--- app/Http/Controllers/Swagger/MainController.php
+++ app/Http/Controllers/Swagger/MainController.php
@@ -7,8 +7,8 @@ use Illuminate\Routing\Controller;
 /**
  * @OA\Info(
  *     title="Avia-back API",
- *     version="1.3.6",
- *     description="Дата обновления: 29.04.2024"
+ *     version="1.3.7",
+ *     description="Дата обновления: 01.05.2024"
  * ),
  *
  * @OA\PathItem(
