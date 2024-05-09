faa443619f65ff7b4beacc41e6b397a0e1fe33d1
LedZeppe1in <tualatin32@mail.ru>
2024-05-09 16:40:33 +0800

    Updated swagger API documentation for malfunction cause rules

--- app/Http/Controllers/Swagger/MainController.php
+++ app/Http/Controllers/Swagger/MainController.php
@@ -7,8 +7,8 @@ use Illuminate\Routing\Controller;
 /**
  * @OA\Info(
  *     title="Avia-back API",
- *     version="1.3.7",
- *     description="Дата обновления: 01.05.2024"
+ *     version="1.3.8",
+ *     description="Дата обновления: 09.05.2024"
  * ),
  *
  * @OA\PathItem(
