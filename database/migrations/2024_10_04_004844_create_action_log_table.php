ff08f564af7de050d27c2ffa46800e433e227cfe
LedZeppe1in <tualatin32@mail.ru>
2024-10-07 10:43:23 +0800

    Added new migration and model for action logging

---
+++ database/migrations/2024_10_04_004844_create_action_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('action_log', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('datetime');
+            $table->text('description');
+            $table->string('client_ip');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('action_log');
+    }
+};
