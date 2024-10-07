0bc3c41c5ec75f414942996fddc9f310b59ae8e4
LedZeppe1in <tualatin32@mail.ru>
2024-10-07 10:44:04 +0800

    Added new method to get client ip address

--- app/Components/Helper.php
+++ app/Components/Helper.php
@@ -155,4 +155,25 @@ class Helper
         }
         return $data;
     }
+
+    /**
+     * Возвращает IP-адрес клиента.
+     *
+     * @return string
+     */
+    public static function getIp() {
+        $keys = [
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'REMOTE_ADDR'
+        ];
+        foreach ($keys as $key)
+            if (!empty($_SERVER[$key])) {
+                $array = explode(',', $_SERVER[$key]);
+                $ip = trim(end($array));
+                if (filter_var($ip, FILTER_VALIDATE_IP))
+                    return $ip;
+            }
+        return '';
+    }
 }
